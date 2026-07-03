cb774ac8cb4aa4034b7465f072c51299cc886ef7
lejdg <f0r.the.0ther.stuff.0815@gmail.com>
2026-07-03 18:57:05 +0200

    Update and rename test7.js to test8.js

---
+++ test8.js
@@ -0,0 +1,26 @@
+console.log("START");
+
+window.addEventListener("load", function () {
+
+    console.log("LOAD");
+
+    console.log("Test gefunden:", $("#test").length);
+    console.log("Draggable:", typeof $.fn.draggable);
+
+    $("#test").draggable({
+
+        containment: "#container",
+
+        start: function(){
+            console.log("START DRAG");
+        },
+
+        stop: function (e, ui) {
+            console.log("STOP DRAG", ui.position);
+        }
+
+    });
+
+    console.log("DRAGGABLE AKTIV");
+
+});
