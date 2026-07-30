4a292d5a49f331a823dca9b66d48edb94422d9b5
lejdg <f0r.the.0ther.stuff.0815@gmail.com>
2026-07-30 20:08:37 +0200

    Update barosort3.js

--- barosort3.js
+++ barosort3.js
@@ -1,5 +1,5 @@
 (function(){
-console.log("Sort BARO VERSION 1.2");
+console.log("Sort BARO VERSION 1.3");
   const RESET_FLAG = "barometer_session_reset_done";
 
   if(!localStorage.getItem(RESET_FLAG)){
